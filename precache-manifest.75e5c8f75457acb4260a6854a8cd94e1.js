0323ded138f0124cf62812bd96ab7da92aec4f10
Tortilla123 <jp568@njit.edu>
2019-11-07 17:42:18 -0600

    Updates

---
+++ precache-manifest.75e5c8f75457acb4260a6854a8cd94e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0d2fe64805148cdea2e933613c16a8",
+    "url": "/Tortilla.github.io/index.html"
+  },
+  {
+    "revision": "3ec0655d8fe0912540b2",
+    "url": "/Tortilla.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b4a305bf4ee9ea520c61",
+    "url": "/Tortilla.github.io/static/js/2.168791a1.chunk.js"
+  },
+  {
+    "revision": "3ec0655d8fe0912540b2",
+    "url": "/Tortilla.github.io/static/js/main.6339d1a0.chunk.js"
+  },
+  {
+    "revision": "536ccccd4068e8da4345",
+    "url": "/Tortilla.github.io/static/js/runtime-main.f9a0f3bb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Tortilla.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
